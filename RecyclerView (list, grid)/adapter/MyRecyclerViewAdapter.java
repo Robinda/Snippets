029756da927c5f768e698aaaf4f1b63e9a2b4e3a
robinda <40255054+Robinda@users.noreply.github.com>
2018-10-03 17:59:22 +0200

    Initial commit

---
+++ RecyclerView (list, grid)/adapter/MyRecyclerViewAdapter.java
@@ -0,0 +1,85 @@
+package com.example.robin.demoapp.snippets;
+
+import java.util.List;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
+    private List<String> values;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        TextView txtHeader;
+        TextView txtFooter;
+        public View layout;
+
+        private ViewHolder(View v) {
+            super(v);
+            layout = v;
+            txtHeader = v.findViewById(R.id.firstLine);
+            txtFooter = v.findViewById(R.id.secondLine);
+        }
+    }
+
+    public void add(int position, String item) {
+        values.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    private void remove(int position) {
+        values.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyRecyclerViewAdapter(List<String> myDataset) {
+        values = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @NonNull
+    @Override
+    public MyRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // create a new view
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(R.layout.row_layout, parent, false);
+
+        // set the view's size, margins, paddings and layout parameters
+        return new ViewHolder(v);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        final String name = values.get(position);
+        holder.txtHeader.setText(name);
+
+        final int pos = position;
+        holder.txtHeader.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remove(pos);    // au clic : suppression de l'item de la liste
+            }
+        });
+
+        holder.txtFooter.setText("Footer: " + name);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return values.size();
+    }
+
+}
