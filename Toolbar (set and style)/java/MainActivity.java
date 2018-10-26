c1f8a1eda496b7485eedc18d47a94b0327814dd2
robinda <40255054+Robinda@users.noreply.github.com>
2018-10-26 02:01:27 +0200

    Format/indent code

--- Toolbar (set and style)/java/MainActivity.java
+++ Toolbar (set and style)/java/MainActivity.java
@@ -6,39 +6,39 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-		
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		
-		// Bind the activity layout
+
+        // Bind the activity layout
         setContentView(R.layout.activity_my_favorites);
-        
-		// Find the toolbar view inside the activity layout
+
+        // Find the toolbar view inside the activity layout
         Toolbar toolbar = findViewById(R.id.toolbar);
-        
-		// Sets the Toolbar to act as the ActionBar for this Activity window.
+
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
         // Make sure the toolbar exists in the activity and is not null
         setSupportActionBar(toolbar);
 
         // Get a support ActionBar corresponding to this toolbar
         ActionBar ab = getSupportActionBar();
-		
+
         // Enable the Up button
         if (ab != null) {
-            ab.setDisplayShowTitleEnabled(false);		// Remove default title text
-            ab.setDisplayHomeAsUpEnabled(true);			// active la flèche de retour vers l'activité précédente
+            ab.setDisplayShowTitleEnabled(false); 	// Remove default title text
+            ab.setDisplayHomeAsUpEnabled(true); 	// active la flèche de retour vers l'activité précédente
 
             // Get access to the custom title view
             TextView tvToolbarTitle = toolbar.findViewById(R.id.toolbar_title);
             TextView tvToolbarNotificationsCount = toolbar.findViewById(R.id.toolbar_count_notif);
-            
-			// Set data to views
-			tvToolbarTitle.setText(R.string.label_my_title);
-			tvToolbarNotificationsCount.setText(getNotificationsCount());
+
+            // Set data to views
+            tvToolbarTitle.setText(R.string.label_my_title);
+            tvToolbarNotificationsCount.setText(getNotificationsCount());
         }
-		
-		// To be continued...
+
+        // To be continued...
     }
 
     @Override
@@ -53,11 +53,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-	/**
-	* Retourne le nombre de notifications non lues
-	* return String Nombre de notifications
-	*/
-	private String getNotificationsCount() {
-		return "99+";
-	}
+    /**
+     * Retourne le nombre de notifications non lues
+     * return String Nombre de notifications
+     */
+    private String getNotificationsCount() {
+        return "99+";
+    }
 }
