c9489312745620b8be2fd24dabc6a7809a07a7d7
robinda <40255054+Robinda@users.noreply.github.com>
2018-10-30 20:47:25 +0100

    Corrections

--- Dialog Box (DialogFragment)/java/activity/MainActivity.java
+++ Dialog Box (DialogFragment)/java/activity/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.robin.demoapp;
 
 import android.app.FragmentManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements MyDialogFragment.DialogListener{
+public class MainActivity extends AppCompatActivity implements DialogYesNoFragment.DialogListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,26 +16,33 @@ public class MainActivity extends AppCompatActivity implements MyDialogFragment.
         setContentView(R.layout.activity_main);
 
         Button btnOpenDialogBox = findViewById(R.id.btn_open_dialog_box);
+        Context context = getApplicationContext();
 
         btnOpenDialogBox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager fm = getFragmentManager();
-                MyDialogFragment dialogBox = MyDialogFragment.newInstance("Confirmation du vol",
+
+                // Get image resource Id (use "drawable" for drawables, "string" for strings, etc.)
+                int iconResourceId = context.getResources().getIdentifier("ic_dialog_box_icon", "drawable", context.getPackageName());
+
+                DialogYesNoFragment dialogBox = DialogYesNoFragment.newInstance(
+                        iconResourceId,
+                        "Confirmation du vol",
                         "Etes-vous sûr de vouloir confirmer le Vol 714 pour Sydney ?",
                         "Confirmer",
                         "Annuler",
                         false);
-						
+
                 dialogBox.show(fm, "tag_dialog_box");
-                dialogBox.setDialogListener(this);				
+                dialogBox.setDialogListener(this);
             }
         });
     }
 
     @Override
     public void onConfirm(int result) {
-        if(result == MyDialogFragment.DIALOG_RESULT_POSITIVE) {
+        if(result == DialogYesNoFragment.DIALOG_RESULT_POSITIVE) {
             Toast.makeText(getApplicationContext(), "Clické oui", Toast.LENGTH_SHORT).show();
         }
         else {
--- Dialog Box (DialogFragment)/java/fragment/MyDialogFragment.java
+++ Dialog Box (DialogFragment)/java/fragment/MyDialogFragment.java
@@ -1,18 +1,23 @@
 package com.example.robin.demoapp;
 
-import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MyDialogFragment extends DialogFragment {
+import com.example.robin.demoapp.R;
 
+public class DialogYesNoFragment extends DialogFragment {
+
+    private final String TAG = DialogYesNoFragment.class.getSimpleName();
+    private static final String ICON_RESOURCE_ID = "icone_resource_id";
     private static final String TITLE = "dialog_title";
     private static final String MESSAGE = "dialog_message";
     private static final String POSITIVE_BUTTON_VALUE = "dialog_positive_button";
@@ -22,35 +27,40 @@ public class MyDialogFragment extends DialogFragment {
     public static final int DIALOG_RESULT_POSITIVE = 1;
     public static final int DIALOG_RESULT_NEGATIVE = 0;
 
+    // Par défaut le résultat de la DialogBox est "négatif"
+    private int mDialogResult = DIALOG_RESULT_NEGATIVE;
+
     private DialogListener mDialogListener;
     private TextView mTvDialogTitle, mTvDialogMessage, mTvDialogActionPositive, mTvDialogActionNegative;
+    private ImageView mIvDialogTitleIcon;
 
     /**
      * Interface de communication entre la DialogBox et l'activité parente
      */
     public interface DialogListener {
         void onConfirm(int result);
-    }	
-	
+    }
+
     /**
      * Initialisation du listener
-     * @param listener
+     * @param listener Listener
      */
     public void setDialogListener(DialogListener listener) {
         this.mDialogListener = listener;
     }
 
-    public MyDialogFragment() {
+    public DialogYesNoFragment() {
         // Empty constructor is required for DialogFragment
         // Make sure not to add arguments to the constructor
         // Use `newInstance` instead as shown below
     }
 
-    public static MyDialogFragment newInstance(String title, String message, String positiveButton,
-                                               String negativeButton, Boolean isCancelable) {
-        MyDialogFragment frag = new MyDialogFragment();
+    public static DialogYesNoFragment newInstance(int iconResourceId, String title, String message,
+                                                  String positiveButton, String negativeButton, Boolean isCancelable) {
+        DialogYesNoFragment frag = new DialogYesNoFragment();
 
         Bundle args = new Bundle();
+        args.putInt(ICON_RESOURCE_ID, iconResourceId);
         args.putString(TITLE, title);
         args.putString(MESSAGE, message);
         args.putString(POSITIVE_BUTTON_VALUE, positiveButton);
@@ -78,7 +88,7 @@ public class MyDialogFragment extends DialogFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_my_dialog, container, true);
+        View v = inflater.inflate(R.layout.fragment_dialog_yes_no, container, true);
 
         // Remove title bar
         getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -92,24 +102,39 @@ public class MyDialogFragment extends DialogFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        mIvDialogTitleIcon = view.findViewById(R.id.iv_dialog_title_icon);
         mTvDialogTitle = view.findViewById(R.id.tv_dialog_title_value);
         mTvDialogMessage = view.findViewById(R.id.tv_dialog_message_value);
         mTvDialogActionPositive = view.findViewById(R.id.tv_button_positive_action);
         mTvDialogActionNegative = view.findViewById(R.id.tv_button_negative_action);
 
+        int iconResourceId = getArguments().getInt(ICON_RESOURCE_ID, -1);
         String title = getArguments().getString(TITLE, "");
         String message = getArguments().getString(MESSAGE, "");
         String positiveButton = getArguments().getString(POSITIVE_BUTTON_VALUE, "");
         String negativeButton = getArguments().getString(NEGATIVE_BUTTON_VALUE, "");
 
-        if(title.isEmpty()) {
+        // Pas d'icone
+        if(iconResourceId == 0) {
+            mIvDialogTitleIcon.setVisibility(View.GONE);
+        }
+        // Icone spécifique
+        else if(iconResourceId > 0) {
+            mIvDialogTitleIcon.setImageResource(iconResourceId);
+        }
+        // Icone par défaut
+        else {
+            mIvDialogTitleIcon.setImageResource(R.drawable.ic_menu_share);
+        }
+
+        if (title.isEmpty()) {
             mTvDialogTitle.setVisibility(View.GONE);
         }
         else {
             mTvDialogTitle.setText(title);
         }
 
-        if(message.isEmpty() || positiveButton.isEmpty() || negativeButton.isEmpty()) {
+        if (message.isEmpty() || positiveButton.isEmpty() || negativeButton.isEmpty()) {
             dismiss();
             return;
         }
@@ -121,20 +146,18 @@ public class MyDialogFragment extends DialogFragment {
         mTvDialogActionNegative.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // On set le résultat à "negatif", qui sera transmis à l'activité au moment du onDetach()
+                mDialogResult = DIALOG_RESULT_NEGATIVE;
                 dismiss();
-                if(mDialogListener != null) {
-                    mDialogListener.onConfirm(DIALOG_RESULT_NEGATIVE);
-                }
             }
         });
 
         mTvDialogActionPositive.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // On set le résultat à "positif", qui sera transmis à l'activité au moment du onDetach()
+                mDialogResult = DIALOG_RESULT_POSITIVE;
                 dismiss();
-                if(mDialogListener != null) {
-                    mDialogListener.onConfirm(DIALOG_RESULT_POSITIVE);
-                }
             }
         });
     }
@@ -142,5 +165,16 @@ public class MyDialogFragment extends DialogFragment {
     @Override
     public void onDetach() {
         super.onDetach();
+
+        /*
+         * On transmet le résultat à l'activité au moment où onDetach est appelé : de cette manière même si on clique à
+         * l'extérieur de la DialogBox (si isCancelable = true), on récupère toujours le résultat (dans ce cas "négatif")
+         */
+        if (mDialogListener != null) {
+            mDialogListener.onConfirm(mDialogResult);
+        }
+        else {
+            Log.e(TAG, "Cannot make dialog callback because listener is null (onDetach() method).");
+        }
     }
 }
